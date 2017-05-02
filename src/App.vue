df6c89313111ff7f7c721cf36ea62ae267427a59
leonard.gong <leonard.gong@silksoftware.com>
2017-05-02 17:40:31 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <el-menu theme="dark" :default-active="activeIndex" mode="horizontal" @select="handleSelect" :router="true">
-      <el-menu-item index="home" :route="{name:'home',query:{id:'hahahahah'}}">处理中心</el-menu-item>
-      <el-submenu index="2">
+      <el-menu-item index="center" :route="{name:'center'}"> 
+        my-life
+      </el-menu-item>
+      <el-submenu index="2" >
         <template slot="title">我的工作台</template>
         <el-menu-item index="2-1">选项1</el-menu-item>
         <el-menu-item index="2-2">选项2</el-menu-item>
         <el-menu-item index="2-3">选项3</el-menu-item>
       </el-submenu>
-      <el-menu-item index="center" :route="{name:'center'}"> 
-        my-life
-      </el-menu-item>
+      
+      <el-menu-item index="home" :route="{name:'home',query:{id:'hahahahah'}}">处理中心</el-menu-item>
     </el-menu>
     <div class="line"></div>
     <router-view></router-view>
@@ -33,5 +34,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+  #app .el-menu--horizontal .el-menu-item,#app .el-menu--horizontal .el-submenu{
+    float: right;
+  }
+  #app .el-menu--horizontal .el-submenu .el-menu-item {
+    float: none;
+  }
 </style>
